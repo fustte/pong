35e490542371b6cb940bbf2e0ac75b5bb09aada6
Fustee <chico_jrf@hotmail.com>
2024-12-17 13:56:18 +0100

    feat: empezando pygame

---
+++ pong.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+pygame.display.set_mode((800, 600))
+
+salir = False
+
+while not salir:
+    # buvle principal (main loop)
+
+    pygame.event.get()
+    
+    pass
+
+pygame.quit()
